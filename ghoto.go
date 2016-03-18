53cba782595c93679850606c7f2c4bca8af5d07c
fukata <tatsuya.fukata@gmail.com>
2016-03-19 03:04:38 +0700

    successful run

--- ghoto.go
+++ ghoto.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"io/ioutil"
+	"regexp"
+	"sync"
 	"github.com/codegangsta/cli"
 )
 
@@ -14,12 +16,12 @@ func main() {
 	app.Usage = "Transfer photo(video)"
 	app.Flags = []cli.Flag {
 		cli.StringFlag {
-			Name: "src, s",
+			Name: "from",
 			Value: "/path/to/src",
 			Usage: "Source directory",
 		},
 		cli.StringFlag {
-			Name: "dst, d",
+			Name: "to",
 			Value: "/path/to/dst",
 			Usage: "Destination directory",
 		},
@@ -37,56 +39,101 @@ func main() {
 			Name: "recursive, r",
 			Usage: "Resursive",
 		},
-
+		cli.BoolFlag {
+			Name: "dry-run",
+			Usage: "Dry Run",
+		},
 	}
 	app.Action = func(c *cli.Context) {
-		fmt.Printf("Hello ghoto src=%s, dst=%s, photo-dir=%s, video-dir=%s, recursive=%s \n", c.String("src"), c.String("dst"), c.String("photo-dir"), c.String("video-dir"), c.Bool("recursive"))
+		fmt.Printf("Hello ghoto from=%s, to=%s, photo-dir=%s, video-dir=%s, recursive=%s, dry-run=%s \n", c.String("from"), c.String("to"), c.String("photo-dir"), c.String("video-dir"), c.Bool("recursive"), c.Bool("dry-run"))
+
+		re := regexp.MustCompile(`(?i)^[^\.].*\.(dng|cr2|jpg|jpeg|arw|orf)$`)
 
 		// options
-		src := c.String("src")
-		dst := c.String("dst")
+		from := c.String("from")
+		to := c.String("to")
+		photoDir := c.String("photo-dir")
+		//videoDir := c.String("video-dir")
 		recursive := c.Bool("recursive")
+		dryRun := c.Bool("dry-run")
 
 		// check path
-		isDir, err := IsDirectory(src)
+		isDir, err := IsDirectory(from)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		if isDir != false {
-			fmt.Errorf("%s is not found.", src)
+			fmt.Errorf("%s is not found.", from)
 		}
 
-		isDir, err = IsDirectory(dst)
+		isDir, err = IsDirectory(to)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		if isDir != false {
-			fmt.Errorf("%s is not found.", dst)
+			fmt.Errorf("%s is not found.", to)
 		}
 
-		fileInfos, readDirErr := ioutil.ReadDir(src + "/")
+		fileInfos, readDirErr := ioutil.ReadDir(from + "/")
 		if readDirErr != nil {
 			log.Fatal(err)
 		}
 
-		for _, fileInfo := range fileInfos {
-			name := (fileInfo).Name()
-			srcPath := src + "/" + name
+		var wg sync.WaitGroup
 
-			log.Printf("file=%s", name)
-			isDir, err = IsDirectory(srcPath)
-			if err != nil {
-				log.Fatal(err)
-			}
+		//fileNames := []string{} 
 
-			if isDir && recursive {
-				// recursive
-			} else {
-				//MoveFile()
-			}
+		for _, fi := range fileInfos {
+			wg.Add(1)
+			go func(fileInfo os.FileInfo) {
+				defer wg.Done()
+				name := (fileInfo).Name()
+				filePath := from + "/" + name
+
+				//log.Printf("file=%s", filePath)
+				isDir, err = IsDirectory(filePath)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if isDir && recursive {
+					// recursive
+				} else {
+					if re.MatchString(name) == false {
+						return
+					}
+
+					//log.Printf("match file=%s", filePath)
+					exif, _ := GetExifData(filePath)
+					dateDirPath, dateDirPathErr := GetDateDirPath(exif["Date/TimeOriginal"])
+					if dateDirPathErr != nil {
+						log.Fatal(dateDirPathErr)
+					}
+
+					dstDir := to + "/" + photoDir + "/" + dateDirPath + "/"
+					if dryRun == false {
+						mkdirErr := os.MkdirAll(dstDir, 0755)
+						if mkdirErr != nil {
+							log.Fatal(mkdirErr)
+						}
+					}
+
+					dstPath := dstDir + name
+					log.Printf("%s -> %s", filePath, dstPath)
+
+					if dryRun == false {
+						moveErr := MoveFile(filePath, dstPath)
+						if moveErr != nil {
+							log.Fatal(moveErr)
+						}
+					}
+				}
+			}(fi)
 		}
+
+		wg.Wait()
 	}
 	app.Run(os.Args)
 }
--- util.go
+++ util.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"bytes"
+	"strings"
+	"time"
 )
 
 func IsDirectory(name string) (isDir bool, err error) {
@@ -12,6 +16,47 @@ func IsDirectory(name string) (isDir bool, err error) {
 	return info.IsDir(), nil
 }
 
+func GetExifData(file string) (map[string]string, error) {
+	cmd := exec.Command("exiftool", file)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	tags := make(map[string]string)
+
+	data := strings.Trim(out.String(), " \r\n")
+	lines := strings.Split(data, "\n")
+
+	for _, line := range lines {
+		k, v := strings.Replace(strings.TrimSpace(line[0:32]), " ", "", -1), strings.TrimSpace(line[33:])
+		tags[k] = v
+	}
+
+	return tags, nil
+}
+
+func GetDateDirPath(date string) (string, error) {
+	t, err := time.Parse("2006:01:02 15:04:05", date)
+	if err != nil {
+		return "", err
+	}
+	return t.Format("2006/01/02"), nil
+}
+
 func MoveFile(src, dst string) (error) {
-	return os.Rename(src, dst)
+	cmd := exec.Command("mv", src, dst)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+	//return os.Rename(src, dst)
 }
